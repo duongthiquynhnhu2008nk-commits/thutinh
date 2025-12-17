c2652f28583096c216235cf2118790888d5c24b8
duongthiquynhnhu2008nk-commits <duongthiquynhnhu2008nk@gmail.com>
2025-12-17 21:50:29 +0700

    Update main.js
    Happy Birthday

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent =" Cảm ơn em đã dành thời gian để nhìn những trò trẻ con của anhh và anh có những điều muốn gởi gắm đến tình iu của anhh❤️. Cảm ơn em đã đến bên anhh những lúc anh cô đơn buồn tủi nhất.Cuộc sống thực sự chẳng có ý nghĩa gì nếu không có em. Em làm cho anh luôn cảm thấy đặc biệt và hoàn hảo. Anh chẳng biết nói gì ngoài lời cảm ơn em, anh rất yêu em💕"
+var letterContent =" Happy Birthday, chúc em sinh nhật vui vẻ ,bước sang tuổi mới có thật nhiều niềm vui luôn luôn cười nếu có khóc hãy khóc vì hạnh phúc.Mỗi ngày luôn rạng rỡ chúc em có tất cả trừ vất vả.Sinh nhật thật hạnh phúc nhá.Yêu Em"
 
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
